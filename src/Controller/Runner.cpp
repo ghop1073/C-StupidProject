5de71a716c79a15eda78be0de1d51bae378faf5f
ghop1073 <mhop1073@740s-pl-109.local>
2016-01-25 13:14:30 -0700

    not hello world

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: mhop1073
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This is how you talk to the console in C++" << endl;
+	return 0;
+}
